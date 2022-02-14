5561dbc9378e2368b9b05cf64a1f8fecc3949d01
ismaillezgioglu <rezna.00@hotmail.com>
2022-02-14 16:32:46 +0300

    .

---
+++ public/FrontTemplate/old/header.php
@@ -0,0 +1,142 @@
+<!doctype html>
+<html lang="tr">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Kavak Kavak</title>
+    <meta name="description" content="Zeus - Lawyers and Law Firm HTML Template" />
+    <meta name="author" content="George_Fx">
+    <meta name="keywords" content="" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="icon" href="images/favicon.png">
+    <link rel="stylesheet" type="text/css" href="css/all.min.css">
+    <link rel="stylesheet" type="text/css" href="css/animate.min.css">
+    <link rel="stylesheet" type="text/css" href="js/lib/slick/slick.css">
+    <link rel="stylesheet" type="text/css" href="js/lib/slick/slick-theme.css">
+    <link rel="stylesheet" type="text/css" href="css/flaticon.css">
+    <link rel="stylesheet" type="text/css" href="css/style_gold.css">
+    <link rel="stylesheet" type="text/css" href="css/responsive.css">
+</head>
+
+<body>
+    
+    <div class="page-loading">
+		<div class="c-leaf">
+			
+			<svg class="c-leaf__icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 498.02 187.17"><g id="Layer_2" data-name="Layer 2"><g id="Layer_1-2" data-name="Layer 1"><path class="cls-1" d="M16.87,165.14H2.14v13.3H0V150.17H2.14v13.06H16.87V150.17H19v28.27H16.87Z"/><path class="cls-1" d="M40.45,178.68c-5.67,0-9.68-3.89-9.68-9.41v-19.1h2.14V169c0,4.68,3,7.78,7.54,7.78S48,173.67,48,169V150.17h2.14v19.1C50.14,174.79,46.13,178.68,40.45,178.68Z"/><path class="cls-1" d="M70.78,162.6,64,170.78v7.66H61.89V150.17H64v17.67l14.34-17.67H81L72.21,161l10.21,17.47H79.88Z"/><path class="cls-1" d="M100.2,178.68c-5.68,0-9.69-3.89-9.69-9.41v-19.1h2.15V169c0,4.68,3,7.78,7.54,7.78s7.55-3.1,7.55-7.78V150.17h2.14v19.1C109.89,174.79,105.88,178.68,100.2,178.68Z"/><path class="cls-1" d="M130.53,162.6l-6.75,8.18v7.66h-2.14V150.17h2.14v17.67l14.33-17.67h2.59L132,161l10.2,17.47h-2.54Z"/><path class="cls-1" d="M181.78,178.44l-2.3-2.78a11,11,0,0,1-7.82,3c-5.88,0-9.13-3.3-9.13-8.42,0-3.93,2.82-6.15,5.59-8-1.27-1.47-2.93-3.45-2.93-6.08,0-3.61,2.7-6.27,6.91-6.27,4,0,6.71,2.7,6.71,6.31,0,2.23-1.24,3.78-3,5,0,0-1.39.95-2.07,1.43l6,7.1a9.37,9.37,0,0,0,1.51-5.67h3.89c-.12,3.61-.91,6.47-2.82,8.69l4.72,5.64Zm-11.43-13.62c-2.07,1.43-3.81,2.74-3.81,5.36a4.8,4.8,0,0,0,5.12,4.8,7.5,7.5,0,0,0,5.4-2.18Zm1.75-11.36a2.63,2.63,0,0,0-2.78,2.78c0,1.16.75,2.23,2.18,3.89.44-.27,1.35-.91,1.35-.91,1.11-.75,2.06-1.63,2.06-2.94A2.74,2.74,0,0,0,172.1,153.46Z"/><path class="cls-1" d="M226.68,175.86a9.57,9.57,0,0,1-7.06,2.58h-9.49V150.17h9.49a9.57,9.57,0,0,1,7.06,2.58c2.86,2.86,2.74,7.35,2.74,11.16S229.54,173,226.68,175.86Zm-1.51-21.6a7.94,7.94,0,0,0-6.07-2.18h-6.83v24.45h6.83a7.94,7.94,0,0,0,6.07-2.18c2.23-2.34,2.11-7.27,2.11-10.44S227.4,156.6,225.17,154.26Z"/><path class="cls-1" d="M254.23,171.57H241.05l-2.46,6.87h-2.3l10.4-28.27h1.91L259,178.44h-2.31Zm-12.5-1.91h11.83L247.64,153Z"/><path class="cls-1" d="M269.52,153.86v24.58h-2.15V150.17h2.15l16.31,24.5v-24.5H288v28.27h-2.15Z"/><path class="cls-1" d="M300.12,150.17h2.15v28.27h-2.15Z"/><path class="cls-1" d="M321.48,178.68a12,12,0,0,1-9.41-3.74l1.51-1.5a9.93,9.93,0,0,0,8,3.33c4.49,0,7.27-2.14,7.27-5.84a5,5,0,0,0-1.55-4c-1.07-.83-1.75-1.19-4.09-1.51l-3.3-.51a9.58,9.58,0,0,1-5-2.07,6.42,6.42,0,0,1-2.22-5.2c0-4.76,3.33-7.74,8.77-7.74a11.14,11.14,0,0,1,8.14,3.06l-1.39,1.39a9,9,0,0,0-6.87-2.62c-4.13,0-6.55,2.22-6.55,5.83a4.57,4.57,0,0,0,1.47,3.7,9.38,9.38,0,0,0,4.21,1.74l3.06.48c2.66.36,3.89.87,5,1.87a6.93,6.93,0,0,1,2.46,5.48C331,175.66,327.4,178.68,321.48,178.68Zm-3.77,8.49,2.86-5.95h2.18l-2.54,5.95Z"/><path class="cls-1" d="M362.73,155.13l-8.58,19.34H352l-8.73-19.34v23.31h-2.15V150.17h2.15l9.84,21.8,9.61-21.8h2.15v28.27h-2.15Z"/><path class="cls-1" d="M391.19,171.57H378l-2.46,6.87h-2.3l10.4-28.27h1.91L396,178.44h-2.3Zm-12.5-1.91h11.83L384.6,153Z"/><path class="cls-1" d="M406.48,153.86v24.58h-2.15V150.17h2.15l16.32,24.5v-24.5h2.14v28.27H422.8Z"/><path class="cls-1" d="M437.09,150.17h2.14v26.36h15v1.91H437.09Z"/><path class="cls-1" d="M463.21,150.17h2.14v28.27h-2.14Z"/><path class="cls-1" d="M486.39,162.6l-6.75,8.18v7.66H477.5V150.17h2.14v17.67L494,150.17h2.58L487.82,161,498,178.44h-2.54Z"/><path class="cls-1" d="M290.23,0,245.06,119.94h-.26V51.29a5.51,5.51,0,1,0-4,0v68.67h-.26L194.8,0Z"/><path class="cls-1" d="M72,119.94,38.43,61,18.26,85.16v34.78H.12V.31H18.26V60.46L67.16.31H89.34L50.53,47.18l42.84,72.76Z"/><path class="cls-1" d="M179.39,119.94l-8.23-24H123.94l-8.23,24H96.39L140.24.31h14.62l43.85,119.63ZM148,27.36,129.32,80.45h36.8Z"/><path class="cls-1" d="M367.41,119.94l-8.24-24H312l-8.24,24H284.4L328.26.31h14.61l43.86,119.63ZM336,27.36,317.34,80.45h36.79Z"/><path class="cls-1" d="M475.61,119.94,442,61l-20.16,24.2v34.78H403.69V.31h18.15V60.46L470.74.31h22.18L454.1,47.18,497,119.94Z"/></g></g></svg>
+
+			<div class="c-leaf__fill"></div>
+		</div>
+    </div>
+
+    <div class="wrapper">
+        
+        <header>
+            <div class="container">
+                <div class="header-content d-flex flex-wrap align-items-center">
+                    <div class="menu-btn">
+                        <a href="#" title="">
+                            <span class="bar1"></span>
+                            <span class="bar2"></span>
+                            <span class="bar3"></span>
+                        </a>
+                    </div><!--menu-btn end-->
+                    <div class="logo">
+                        <a href="index.php" title="">
+                            <img src="images/logo.png" alt="">
+                        </a>
+                    </div><!-- logo end-->
+                    <nav>
+                        <ul>
+                            <li><a class="active" href="index.html" title="">Anasayfa</a></li>
+                            <li><a href="#" title="">Hakkımızda</a></li>
+                            <li><a href="#" title="">Hizmetlerimiz</a></li>
+                            <li><a href="#" title="">Ekibimiz</a></li>
+                            <li><a href="#" title="">Haberler</a></li>
+                            <li><a href="#" title="">İletişim</a></li>
+                        </ul>
+                    </nav><!--navigation end-->
+                    <ul class="contact-head-info ml-auto">
+                        <li>
+                            <img src="images/phone.svg" alt="">
+                            <span>+90 (312) 474 0707</span>
+                        </li>
+                        <li>
+                            <img src="images/mail.svg" alt="">
+                            <a href="mailto:example@example.com" title="">info@kavak-kavak.av.tr</a>
+                        </li>
+                    </ul><!--contact-head-info end-->
+                </div><!--header-content end-->
+            </div>
+        </header><!--header end-->
+
+        <div class="burger-menu">
+            <a href="#" title="" class="close-menu">
+                <i class="flaticon-close"></i>
+            </a>
+            <div class="menu-middle">
+                <div class="container">
+                    <div class="main-menu">
+                        <div class="row">
+                            <div class="col-md-4">
+                                <div class="menu-widget">
+                                    <h4>Sosyal Medya</h4>
+                                    <ul>
+                                        <li><a href="#" title="">Twitter</a></li>
+                                        <li><a href="#" title="">Linkedin</a></li>
+                                        <li><a href="#" title="">Instagram</a></li>
+                                        <li><a href="#" title="">Facebook</a></li>
+                                        <li><a href="#" title="">Telegram</a></li>
+                                    </ul>
+                                </div><!--menu-widget end-->
+                            </div>
+                            <div class="col-md-4">
+                                <div class="menu-widget">
+                                    <h4>Kavak-Kavak</h4>
+                                    <ul>
+                                        <li><a href="#" title="">Hakkımızda</a></li>
+                                        <li><a href="#" title="">Hizmetlerimiz</a></li>
+                                        <li><a href="#" title="">Ekibimiz</a></li>
+                                        <li><a href="#" title="">Haberler</a></li>
+                                        <li><a href="#" title="">İletişim</a></li>
+                                    </ul>
+                                </div><!--menu-widget end-->
+                            </div>
+                            <div class="col-md-4">
+                                <div class="menu-widget">
+                                    <h4>Hizmetlerimiz</h4>
+                                    <ul>
+                                        <li><a href="#" title="">Aile Hukuku</a></li>
+                                        <li><a href="#" title="">Vatandaşlık Hukuku</a></li>
+                                        <li><a href="#" title="">Bankacılık ve Finans Hukuku</a></li>
+                                        <li><a href="#" title="">Gayrimenkul Hukuku</a></li>
+                                        <li><a href="#" title="">İdare ve Vergi Hukuku</a></li>
+                                    </ul>
+                                </div><!--menu-widget end-->
+                            </div>
+                        </div>
+                    </div><!--main-menu end-->
+                </div>
+            </div><!--menu-middle end-->
+            <div class="menu-footer">
+                <div class="container">
+                    <div class="row align-items-center">
+                        <div class="col-md-4">
+                            <div class="menu-footer-logo">
+                                <img src="images/logo.png" alt="">
+                            </div><!--menu-footer-logo end-->
+                        </div>
+                        <div class="col-md-8">
+                            <ul class="contact-menu-links">
+                                <li>
+                                    <p>Ehlibeyt Mahallesi, Ceyhun Atuf Kansu Caddesi, Cevat Muratal İş Merkezi No:112/37 Balgat, Çankaya/ANKARA 06520 - Türkiye</p>
+                                </li>
+                                <li><span>+90 (312) 474 0707</span></li>
+                                <li><a href="example@exmaple.com" title="">info@kavak-kavak.av.tr</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div><!--menu-footer end-->
+        </div><!--burger-menu end-->
---
+++ public/FrontTemplate/old/index.php
@@ -0,0 +1,150 @@
+<?php include('header.php'); ?>
+
+        <section class="main-banner overlay">
+            <div class="container">
+                <div class="main-banner-content">
+                    <div class="social-links">
+                        <ul>
+                            <li><a href="#" title=""><i class="fab fa-linkedin"></i></a></li>
+                            <li><a href="#" title=""><i class="fab fa-twitter"></i></a></li>
+                            <li><a href="#" title=""><i class="fab fa-facebook-f"></i></a></li>
+                        </ul>
+                    </div>
+                    <div class="banner-text">
+                        <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h2>
+						<div class="banner-actions">
+                        	<a href="#" title="">Hizmetler</a>
+                        	<a href="#" title="">Ekibimiz</a>
+                        	<a href="#" title="">İletişim</a>
+						</div>
+                    </div>
+                    <div class="social-links"></div>
+                </div><!--main-banner-content end-->
+            </div>
+        </section><!--main-banner end-->
+
+        <section class="block">
+            <div class="container">
+                <div class="about-section">
+                    <div class="row align-items-center">
+                        <div class="col-lg-6">
+                            <div class="abt-img overlay">
+                                <img src="images/resources/abt-img.jpg" alt="" class="w-100">
+                                <h2>Şirketimizin tarihçesi</h2>
+                                <a href="https://www.youtube.com/watch?v=F7vNdEqwrho" title="" class="play-btn html5lightbox"><i class="fa fa-play"></i></a>
+                            </div><!--abt-img end-->
+                        </div>
+                        <div class="col-lg-6">
+                            <div class="about-text wow fadeInUp" data-wow-duration="1000ms">
+                                <h2>Hakkımızda</h2>
+                                <p>Kavak Hukuk ve Danışmanlık Ofisi, Av. Süleyman KAVAK tarafından 2013 yılında Ankara’da kurulmuştur.</p>
+                                <p>Kuruluşundan itibaren güçlü Avukat ve danışman kadrosu ile çeşitli alanlarda hukuk hizmeti sunmaktadır. İlgili uyuşmazlık konuları üzerinde çözüme ulaşmak amacıyla çalışma alanlarına göre uzmanlaşmış  departmanlara ayrılarak organize olmuş ve farklı hukuk disiplinleri konusunda müvekkillerine eksiksiz ve zamanında hizmet sunmaya özen gösterir.</p>
+                                <a href="#" title="" class="btn-default">Devamı</a>
+                            </div><!--about-text end-->
+                        </div>
+                    </div>
+                </div><!--about-section end-->
+            </div>
+        </section><!--sec-block end-->
+
+        <section class="block overlay">
+            <div class="fixed-bg bg1"></div>
+            <div class="container">
+                <div class="section-title">
+                    <h2 class="h-title">Hizmetler</h2>
+                </div><!--section-title end-->
+                <div class="services">
+                    <div class="row">
+                        <div class="col-lg-3 col-md-6 col-sm-6">
+                            <div class="service-col wow fadeIn" data-wow-duration="1000ms" data-wow-delay="0ms">
+                                <h2>01<span>.</span></h2>
+                                <h4>Aile Hukuku</h4>
+                                <p>Aenean tincidunt consequat ante, nec ultricies lacus luctus quis. Pellentesque et dignissim.</p>
+                            </div><!--service-col end-->
+                        </div>
+                        <div class="col-lg-3 col-md-6 col-sm-6">
+                            <div class="service-col wow fadeIn" data-wow-duration="1000ms" data-wow-delay="200ms">
+                                <h2>02<span>.</span></h2>
+                                <h4>Vatandaşlık Hukuku</h4>
+                                <p>Aenean tincidunt consequat ante, nec ultricies lacus luctus quis. Pellentesque et dignissim.</p>
+                            </div><!--service-col end-->
+                        </div>
+                        <div class="col-lg-6 col-md-6 col-sm-6">
+                            <div class="smpl-txt">
+                                <h4>Lorem ipsum dolor? Sit amet!</h4>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer semper metus mi, sit amet cursus ante viverra nec. Vivamus luctus, ipsum eget ornare convallis, urna justo rutrum diam, eget tempus lorem ipsum a magna.</p>
+                                <p>Vivamus tempor sit amet ex ac sodales. In sit amet volutpat velit. Ut faucibus elit sed velit imperdiet, ac varius mauris faucibus.</p>
+                                <p>Phasellus dapibus dignissim metus ut consectetur. Phasellus vestibulum malesuada erat, id vehicula leo mattis interdum. Etiam aliquet erat arcu, eu faucibus diam cursus at.</p>
+                            </div><!--smpl-txt end-->
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-lg-3 col-md-6 col-sm-6">
+                            <div class="service-col wow fadeIn" data-wow-duration="1000ms" data-wow-delay="400ms">
+                                <h2>03<span>.</span></h2>
+                                <h4>Bankacılık ve Finans Hukuku</h4>
+                                <p>Aenean tincidunt consequat ante, nec ultricies lacus luctus quis. Pellentesque et dignissim.</p>
+                            </div><!--service-col end-->
+                        </div>
+                        <div class="col-lg-3 col-md-6 col-sm-6">
+                            <div class="service-col wow fadeIn" data-wow-duration="1000ms" data-wow-delay="600ms">
+                                <h2>04<span>.</span></h2>
+                                <h4>Ceza Hukuku ve Ceza Muhakemesi Hukuku</h4>
+                                <p>Aenean tincidunt consequat ante, nec ultricies lacus luctus quis. Pellentesque et dignissim.</p>
+                            </div><!--service-col end-->
+                        </div>
+                        <div class="col-lg-3 col-md-6 col-sm-6">
+                            <div class="service-col wow fadeIn" data-wow-duration="1000ms" data-wow-delay="800ms">
+                                <h2>05<span>.</span></h2>
+                                <h4>Gayrimenkul Hukuku</h4>
+                                <p>Aenean tincidunt consequat ante, nec ultricies lacus luctus quis. Pellentesque et dignissim.</p>
+                            </div><!--service-col end-->
+                        </div>
+                        <div class="col-lg-3 col-md-6 col-sm-6">
+                            <div class="service-col wow fadeIn" data-wow-duration="1000ms" data-wow-delay="1000ms">
+                                <h2>06<span>.</span></h2>
+                                <h4>İdare ve Vergi Hukuku</h4>
+                                <p>Aenean tincidunt consequat ante, nec ultricies lacus luctus quis. Pellentesque et dignissim.</p>
+                            </div><!--service-col end-->
+                        </div>
+                        <div class="col-lg-3 col-md-6 col-sm-6">
+                            <div class="service-col wow fadeIn" data-wow-duration="1000ms" data-wow-delay="1000ms">
+                                <h2>07<span>.</span></h2>
+                                <h4>İş ve Sosyal Güvenlik Hukuku</h4>
+                                <p>Aenean tincidunt consequat ante, nec ultricies lacus luctus quis. Pellentesque et dignissim.</p>
+                            </div><!--service-col end-->
+                        </div>
+                        <div class="col-lg-3 col-md-6 col-sm-6">
+                            <div class="service-col wow fadeIn" data-wow-duration="1000ms" data-wow-delay="1000ms">
+                                <h2>08<span>.</span></h2>
+                                <h4>Sözleşme Hukuku</h4>
+                                <p>Aenean tincidunt consequat ante, nec ultricies lacus luctus quis. Pellentesque et dignissim.</p>
+                            </div><!--service-col end-->
+                        </div>
+                        <div class="col-lg-3 col-md-6 col-sm-6">
+                            <div class="service-col wow fadeIn" data-wow-duration="1000ms" data-wow-delay="1000ms">
+                                <h2>09<span>.</span></h2>
+                                <h4>İcra ve İflas Hukuku</h4>
+                                <p>Aenean tincidunt consequat ante, nec ultricies lacus luctus quis. Pellentesque et dignissim.</p>
+                            </div><!--service-col end-->
+                        </div>
+                        <div class="col-lg-3 col-md-6 col-sm-6">
+                            <div class="service-col wow fadeIn" data-wow-duration="1000ms" data-wow-delay="1000ms">
+                                <h2>10<span>.</span></h2>
+                                <h4>Sağlık Hukuku</h4>
+                                <p>Aenean tincidunt consequat ante, nec ultricies lacus luctus quis. Pellentesque et dignissim.</p>
+                            </div><!--service-col end-->
+                        </div>
+                        <div class="col-lg-3 col-md-6 col-sm-6">
+                            <div class="service-col wow fadeIn" data-wow-duration="1000ms" data-wow-delay="1000ms">
+                                <h2>11<span>.</span></h2>
+                                <h4>Anayasa Mahkemesine ve Avrupa İnsan Hakları Mahkemesine Bireysel Başvuru</h4>
+                                <p>Aenean tincidunt consequat ante, nec ultricies lacus luctus quis. Pellentesque et dignissim.</p>
+                            </div><!--service-col end-->
+                        </div>
+                    </div>
+                </div><!--services end-->
+            </div>
+        </section>
+
+<?php include('footer.php'); ?>
